fceca55074e94a09e67db0db0ea3f5d7c1b79553
hashd <gitk@danduprolu.com>
2015-06-22 02:28:07 +0530

    Primes module with newer prime finder implementation

---
+++ practice/elixir/prime_finder.ex
@@ -0,0 +1,14 @@
+defmodule Primes do
+  def stupid_find(n) when n >= 2 do
+    (for i <- 2..n, j <- 2..trunc(:math.sqrt(i)), j > 1, do: {i,j, rem(i,j)}) |> 
+      (Enum.chunk_by &(Tuple.to_list(&1) |> List.first)) |> 
+      Enum.map(&(List.zip(&1))) |> 
+      Enum.map(fn ([a,_,c]) -> {(Tuple.to_list(a) |> List.first), (Tuple.to_list(c) |> Enum.reduce(1, fn (x,acc) -> x * acc end))} end) |> 
+      Enum.filter(fn ({_,b}) -> b !== 0 end) |>
+      Enum.map(fn ({a, _}) -> a end)
+  end
+
+  def find(n) when n >= 2 do
+    (2..n |> Enum.to_list) -- (for i <- 2..trunc(:math.sqrt(n)), j <- i..trunc(:math.sqrt(n)), j>=i, i*j < 10000, do: i*j)
+  end
+end
--- practice/elixir/prime_finder.exs
+++
@@ -1,5 +0,0 @@
-(for i <- 3..10000, j <- 2..trunc(:math.sqrt(i)), j > 1, do: {i,j, rem(i,j)}) |> 
-  (Enum.chunk_by &(Tuple.to_list(&1) |> List.first)) |> 
-  Enum.map(&(List.zip(&1))) |> 
-  Enum.map(fn ([a,_,c]) -> {(Tuple.to_list(a) |> List.first), (Tuple.to_list(c) |> Enum.reduce(1, fn (x,acc) -> x * acc end))} end) |> 
-  Enum.filter(fn ({a,b}) -> b !== 0 end) 
