3d1a90264160d049b8d128155c3cd41dfcbdf62b
hashd <kd@danduprolu.com>
2015-09-08 05:06:56 +0530

    Elixir update 29: Permutations and QSort using Elixir

---
+++ practice/elixir/comprehensions.ex
@@ -0,0 +1,17 @@
+defmodule Comprehensions do
+  def qsort([]), do: []
+  def qsort([p | r]) do
+    lt = for ele <- r, ele  < p, do: ele
+    gt = for ele <- r, ele >= p, do: ele
+    
+    qsort(lt) ++ [p] ++ qsort(gt)
+  end
+
+  def perms([]), do: [[]]
+  def perms(list) do
+    for ele <- list, rest <- perms(list -- [ele]), do: [ele | rest]
+  end
+end
+
+1..100 |> Enum.shuffle |> Comprehensions.qsort
+1..4 |> Enum.shuffle |> Comprehensions.perms
