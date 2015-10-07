49ec8f8c3933ce6ddd074c39b1bee94d06a9723c
hashd <kd@danduprolu.com>
2015-10-08 03:44:44 +0530

    React update: Separation of concerns

--- sample-projects/react/storekeeper/src/components/orders.js
+++ sample-projects/react/storekeeper/src/components/orders.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import PageHeader from './page_header'
 import OrdersTable from './orders/orders_table'
+import { toTitleCase } from '../lib/formatters'
 import ORDERS_DATA from '../../mock/orders'
 
+const STATUSES = ['all', 'open', 'shipped']
+
 class Orders extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { selectedStatus: 'all' }
+  }
+
   render() {
+    const { selectedStatus } = this.state
+    const statuses = STATUSES.map((status, idx) => {
+      const className = (status === selectedStatus)? 'selected status': 'status';
+      return (
+        <a key={idx} className={className} onClick={this.handleStatusClick.bind(this, status)}>
+          {toTitleCase(status)}
+        </a>
+      )
+    })
+    const orders = ORDERS_DATA.filter((order) => order.orderStatus === selectedStatus || selectedStatus === 'all')
+
     return (
       <div className="orders">
         <PageHeader>
-          <h1>Orders</h1>
+          <h4>Orders</h4>
+          <nav>
+            {statuses}
+          </nav>
         </PageHeader>
-        <OrdersTable orders={ORDERS_DATA} />
+        <OrdersTable orders={orders} />
       </div>
     )
+  }
+
+  handleStatusClick(status) {
+    this.setState({ selectedStatus: status })
   }
 }
 
